5458a4715658594c50c7b14e021a48a74c58ab39
c82957a <c82957a@NB66224.serasa.intranet>
2020-03-16 17:50:12 -0300

    Authentication in AD with Spring LDAP

---
+++ authentication/src/main/java/com/ldap/authentication/springconfig/WebSecurityConfig.java
@@ -0,0 +1,90 @@
+package com.ldap.authentication.springconfig;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.DirContextOperations;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.userdetails.LdapUserDetails;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsMapper;
+import org.springframework.security.ldap.userdetails.UserDetailsContextMapper;
+
+import com.ldap.authentication.springconfig.model.CustomLdapUserDetails;
+import com.ldap.authentication.springconfig.utils.LdapHelper;
+
+//@Slf4j
+@EnableWebSecurity
+@Configuration
+@ConfigurationProperties(prefix = "spring.ldap")
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	LdapHelper ldapHelper;
+
+	@Value("${spring.ldap.urls}")
+	private String LDAP_URL;
+
+	@Value("${spring.ldap.base-dn}")
+	private String LDAP_BASE_DN;
+
+	@Value("${spring.ldap.search}")
+	private String LDAP_BASE_SEARCH;
+
+	@Value("${spring.ldap.search-filter}")
+	private String LDAP_SEARCH_FILTER;
+
+	@Value("${spring.ldap.username}")
+	private String LDAP_USER;
+
+	@Value("${spring.ldap.password}")
+	private String LDAP_PASSWORD;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().fullyAuthenticated().and().formLogin();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.ldapAuthentication()
+			.userDetailsContextMapper(userDetailsContextMapper())
+			.userSearchFilter(LDAP_SEARCH_FILTER).contextSource(getContext());
+	}
+
+	private DefaultSpringSecurityContextSource getContext() {
+		DefaultSpringSecurityContextSource defaultSpringSecurityContextSource = new DefaultSpringSecurityContextSource(Arrays.asList(LDAP_URL), LDAP_BASE_DN);
+		defaultSpringSecurityContextSource.setBase(LDAP_BASE_SEARCH + "," + LDAP_BASE_DN);
+		defaultSpringSecurityContextSource.setUserDn(LDAP_USER + "," + LDAP_BASE_SEARCH + "," + LDAP_BASE_DN);
+		defaultSpringSecurityContextSource.setPassword(LDAP_PASSWORD);
+		defaultSpringSecurityContextSource.setAnonymousReadOnly(false);
+		defaultSpringSecurityContextSource.setPooled(true);
+		defaultSpringSecurityContextSource.afterPropertiesSet();
+		return defaultSpringSecurityContextSource;
+	}
+
+	@Bean
+	public UserDetailsContextMapper userDetailsContextMapper() {
+		return new LdapUserDetailsMapper() {
+
+			@Override
+			public UserDetails mapUserFromContext(DirContextOperations ctx, String username, Collection<? extends GrantedAuthority> authorities) {
+				UserDetails details = super.mapUserFromContext(ctx, username, authorities);
+				return new CustomLdapUserDetails((LdapUserDetails) details); 
+				} 
+			};
+
+	}
+
+}
