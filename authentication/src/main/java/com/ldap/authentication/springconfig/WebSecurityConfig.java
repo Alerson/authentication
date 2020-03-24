b163b71ed7fb3ab573c475b49012790b6b85623a
c82957a <c82957a@NB66224.serasa.intranet>
2020-03-24 01:18:42 -0300

    Getting better login screen.

--- authentication/src/main/java/com/ldap/authentication/springconfig/WebSecurityConfig.java
+++ authentication/src/main/java/com/ldap/authentication/springconfig/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.ldap.core.DirContextOperations;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.GrantedAuthority;
@@ -52,7 +53,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().fullyAuthenticated().and().formLogin();
+		http.authorizeRequests().anyRequest().fullyAuthenticated().and()
+			.formLogin().loginPage("/login").permitAll();
 	}
 
 	@Override
@@ -61,6 +63,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.ldapAuthentication()
 			.userDetailsContextMapper(userDetailsContextMapper())
 			.userSearchFilter(LDAP_SEARCH_FILTER).contextSource(getContext());
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**").anyRequest();
 	}
 
 	private DefaultSpringSecurityContextSource getContext() {
